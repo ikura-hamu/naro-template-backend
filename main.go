f76b46ac1db6ff14a585e2627faf0456b09440af
ikura-hamu <104292023+ikura-hamu@users.noreply.github.com>
2023-06-03 18:40:34 +0900

    サーバーからデータベースを扱う

--- main.go
+++ main.go
@@ -1,45 +1,57 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
 )
 
 type City struct {
-	ID          int    `json:"ID,omitempty" db:"ID"`
-	Name        string `json:"name,omitempty" db:"Name"`
+	ID          int    `json:"id,omitempty"  db:"ID"`
+	Name        string `json:"name,omitempty"  db:"Name"`
 	CountryCode string `json:"countryCode,omitempty"  db:"CountryCode"`
 	District    string `json:"district,omitempty"  db:"District"`
 	Population  int    `json:"population,omitempty"  db:"Population"`
 }
 
+var (
+	db *sqlx.DB
+)
+
 func main() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True&loc=Asia%%2FTokyo&charset=utf8mb4",
 		os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE"))
-	db, err := sqlx.Open("mysql", dsn)
+	_db, err := sqlx.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Println("conntected")
+	db = _db
 
-	newCity := City{
-		Name:        "Oookayama",
-		CountryCode: "JPN",
-		District:    "Tokyo-to",
-		Population:  5000,
+	e := echo.New()
+
+	e.GET("/cities/:cityName", getCityInfoHandler)
+
+	e.Start(":3000")
+}
+
+func getCityInfoHandler(c echo.Context) error {
+	cityName := c.Param("cityName")
+	fmt.Println(cityName)
+
+	var city City
+	if err := db.Get(&city, "SELECT * FROM city WHERE Name=?", cityName); errors.Is(err, sql.ErrNoRows) {
+		return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("No such city Name = %s", cityName))
+	} else if err != nil {
+		log.Fatalf("failed to get city: %s", err)
 	}
-	result, err := db.Exec("INSERT INTO city (Name, CountryCode, District, Population) VALUES (?,?,?,?)", newCity.Name, newCity.CountryCode, newCity.District, newCity.Population)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	id, _ :=  result.LastInsertId()
-
-	fmt.Printf("%sのIDは%d\n", newCity.Name, id)
+	return c.JSON(http.StatusOK, city)
 }
